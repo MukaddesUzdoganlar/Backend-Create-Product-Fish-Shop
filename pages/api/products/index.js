40e8628311ad8f42193356bb435203be1af33396
Mukaddes Uzdoganlar <m_uzdoganlar@web.de>
2023-09-29 16:02:34 +0200

    solution

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -8,4 +8,18 @@ export default async function handler(request, response) {
     const products = await Product.find();
     return response.status(200).json(products);
   }
+  if (request.method === "POST") {
+    try {
+      const newProduct = request.body;
+      await Product.create(newProduct);
+
+      response.status(201).json({ status: "Product created!" });
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ message: "Internal server error." });
+    }
+    return;
+  }
+
+  response.status(405).json({ message: "Method not allowed." });
 }
